a1e74bea0ff6713b67c1fbc453189793498e53ed
user.name <avi26022007@gmail.com>
2025-01-30 16:26:33 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
-// import SplitText from "./SplitText";
-// import SplitText from "./SplitText";
-import SplitText from './blocks/TextAnimations/SplitText/SplitText';
+import React from 'react';
+import Navbar from './components/Navbar';
+
 function App() {
-  const handleAnimationComplete = () => {
-    console.log('All letters have animated!');
-  };
-  
   return (
-    <div className='bg-green-200'>
-      <SplitText
-        text="Hello,aaaaaaaaaaaahhhhhhh Tailwind!"
-        className="text-2xl text-black  font-semibold text-center"
-        delay={150}
-        animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
-        animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-        easing="easeOutCubic"
-        threshold={0.2}
-        rootMargin="-50px"
-        onLetterAnimationComplete={handleAnimationComplete}
-      />
+    <div className="relative w-full h-screen">
+
+      {/* Main Content */}
+      <Navbar />
+      <div className="relative z-10 text-white p-10">
+        <h1 className="text-4xl font-bold">Hello, World!</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar-container hover:scale-105 transition duration-400 fixed top-3 left-1/2 transform -translate-x-1/2 w-1/2 h-16 p-1  bg-white/30 backdrop-blur-sm shadow-lg border border-gray-200 rounded-full z-50">
+      <div className="flex justify-between items-center h-full px-6">
+        {/* Left Side Content */}
+        <div className="flex space-x-4">
+          <span className="text-3xl font-bold text-[#002f34]">
+            ChainXchange
+          </span>
+        </div>
+
+        {/* Right Side Content */}
+        <div className="flex space-x-4">
+          <button
+            
+            className="text-sm font-medium text-gray-700 hover:text-gray-900 transition"
+          >
+            My Profile
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
