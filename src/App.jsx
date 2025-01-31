388ec4ae1c96940c8f26d897d932c2bcc2fdf2af
user.name <avi26022007@gmail.com>
2025-01-31 21:57:19 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function App() {
       <Navbar />
       <div className="relative z-10  text-white p-10">
         <h1 className="text-4xl  mt-10 font-bold">Hello, World!</h1>
-        <div className='flex  border rounded-full'>
-
-      <ShinyText text="Just some shiny text!" disabled={false} speed={2} className='ml-10' />
-        </div>
+        
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
+import ShinyText from '../blocks/TextAnimations/ShinyText/ShinyText'
 const Navbar = () => {
 
   return (
-    <div className="navbar-container hover:scale-105 transition duration-400 fixed top-3 left-1/2 transform -translate-x-1/2 w-3/4 h-16 p-1 bg-[#14071b]/30 backdrop-blur-sm shadow-lg border border-[#14071b] rounded-full z-50">
+    <div className="navbar-container hover:scale-105 transition duration-400 fixed mt-1 top-3 left-1/2 transform -translate-x-1/2 w-3/4 h-16 p-1 bg-[#14071b]/30 backdrop-blur-sm shadow-lg border border-[#14071b] rounded-full z-50">
       <div className="flex justify-between items-center h-full px-6">
         {/* Left Side Content */}
         <div className="flex space-x-4">
-          <span className="text-3xl font-bold text-[#002f34]">
-            ChainXchange
+          <span className="text-3xl font-bold text-[#491d63]">
+          <ShinyText text="Avi Srivastava" disabled={false} speed={2} className='font-bold' />
           </span>
         </div>
 
         {/* Right Side Content */}
         <div className="flex space-x-4">
-          <button
-            
-            className="text-sm font-medium text-gray-700 hover:text-gray-900 transition"
-          >
-            My Profile
-          </button>
+        <div className='flex gap-x-5  '>
+            <ShinyText text="Just some shiny text!" disabled={false} speed={4} className='' />
+            <ShinyText text="Just some shiny text!" disabled={false} speed={5} className='' />
+            <ShinyText text="Just some shiny text!" disabled={false} speed={4} className='' />
+            <ShinyText text="Just some shiny text!" disabled={false} speed={5} className='' />
+        </div>
         </div>
       </div>
     </div>
