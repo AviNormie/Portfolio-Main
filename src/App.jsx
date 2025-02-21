da061113ff14146c5a26eb4ec7b0b8916ed1d752
user.name <avi26022007@gmail.com>
2025-02-21 23:27:33 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import First from './pages/First'
+// import First from './pages/First'
 import Test from './pages/Test'
-import CompTesting from './pages/CompTesting'
+// import CompTesting from './pages/CompTesting'
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<First/>} />
-      <Route path='/a' element={<Test/>} />
-      <Route path='/c' element={<CompTesting/>} />
+      <Route path='/' element={<Test/>} />
+      {/* <Route path='/a' element={<First/>} /> */}
+      {/* <Route path='/c' element={<CompTesting/>} /> */}
       
       </Routes>
     </BrowserRouter>
